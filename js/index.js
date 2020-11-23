6f14e48eeb62bdbd4f835451236b99d8f580ec69
mkoenke <mkoenke@sas.upenn.edu>
2020-11-23 16:01:47 -0500

    completed

--- js/index.js
+++ js/index.js
@@ -3,44 +3,18 @@ document.addEventListener("DOMContentLoaded", function() {});
 //DOM elements
 const bookUl = document.querySelector("ul")
 const showPanel = document.querySelector("#show-panel")
+let currentBook
 //event listeners
 
 bookUl.addEventListener("click", fetchBook)
 
-//get book to like
 
-function getBookToLike(event){
-    const id = event.target.dataset.id
-    //  fetch(`http://localhost:3000/books/${id}`)
-    // .then(response => response.json())
-    // .then(book => {
-    //     updateUsers(book)
-    //     console.log('Book to update:', book);
-    //   })
-    if (event.target.textContent === "Like"){
-        event.target.textContent = "Unlike"
-        fetch(`http://localhost:3000/books/${id}`)
-        .then(response => response.json())
-        .then(book => {
-            likeBook(book)
-            console.log('Book to like:', book);
-          })
-    } else if (event.target.textContent === "Unlike"){
-        event.target.textContent = "Like"
-        fetch(`http://localhost:3000/books/${id}`)
-        .then(response => response.json())
-        .then(book => {
-            unlikeBook(book)
-            console.log('Book to Unlike:', book);
-          })
-    }
-
-    }
 
     //update users that like the book
 
 function likeBook(book){
     const currentUser = {id:1, username:"pouros"}
+    book = currentBook
     if (book.users.some(obj => obj.username === currentUser.username)){
         book.users
     } else if (book.users.push(currentUser))
@@ -67,13 +41,14 @@ function unlikeBook(book){
     const currentUser = {id:1, username:"pouros"}
     if (book.users.some(obj => obj.username === currentUser.username)){
         book.users = book.users.filter(obj => obj.username !== currentUser.username)
-    } else if (book.users)
+        // debugger
+    
     fetch(`http://localhost:3000/books/${book.id}`, {
     method: 'PATCH', 
     headers: {
         'Content-Type': 'application/json',
     },
-    body: JSON.stringify(console.log({users:book.users})),
+    body: JSON.stringify({users:book.users}),
     })
     .then(response => response.json())
     .then(updatedBook => {
@@ -83,6 +58,7 @@ function unlikeBook(book){
     .catch((error) => {
     console.error('Error:', error);
     });
+}
 }
     
 
@@ -106,7 +82,14 @@ function displayBookInfo(book){
     likeBtn.dataset.id = book.id
     showPanel.append(img, h2Title, h3Subtitle, h3Author, p, list, likeBtn)
     // let users = book.users
-    likeBtn.addEventListener("click", getBookToLike)
+    likeBtn.addEventListener("click", function(){
+        if (likeBtn.textContent === "Like"){
+            likeBtn.textContent = "Unlike"
+            likeBook(book)
+        } else {likeBtn.textContent = "Like"
+            unlikeBook(book)
+    }
+    })
     
     //create user list
     book.users.forEach(user =>{
@@ -114,10 +97,7 @@ function displayBookInfo(book){
         li.textContent = user.username
         list.append(li)
     })
-    // likeBtn.addEventListener("click", function(){
-    //     const currentUser = {id:1, username:"pouros"}
-    //     debugger
-    // })
+   
 }
 
 //fetch book info
@@ -128,6 +108,7 @@ function fetchBook(event){
         fetch(`http://localhost:3000/books/${id}`)
         .then(response => response.json())
         .then(returnedBook => {
+            currentBook = returnedBook
             displayBookInfo(returnedBook)
             console.log('Success:', returnedBook);
         })
@@ -158,6 +139,6 @@ function initialize(){
         renderBooks(booksArray)
         console.log('Success:', booksArray);
       })
-    // .then(booksArray => renderBookList(booksArray));
+    
 }
 initialize()
